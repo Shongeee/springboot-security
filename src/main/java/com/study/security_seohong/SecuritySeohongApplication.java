7526646094d5932d036881da42a90d71b5d7ef64
Shongeee <hongeever@gmail.com>
2022-08-08 21:51:34 +0900

    220808

---
+++ src/main/java/com/study/security_seohong/SecuritySeohongApplication.java
@@ -0,0 +1,13 @@
+package com.study.security_seohong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritySeohongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritySeohongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/security_seohong/SecuritySeohongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.security_seohong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritySeohongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
